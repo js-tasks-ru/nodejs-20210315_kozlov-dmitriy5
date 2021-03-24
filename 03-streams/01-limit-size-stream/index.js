8c9409b3e2583cef3b38f11217070790ba0f2272
Kozlov Dmitriy <siderwinder@icloud.com>
2021-03-24 16:27:59 +0300

    HW #3 task 1 final

--- 03-streams/01-limit-size-stream/index.js
+++ 03-streams/01-limit-size-stream/index.js
@@ -8,6 +8,6 @@ limitedStream.pipe(outStream);
 
 limitedStream.write('hello'); // 'hello' - это 5 байт, поэтому эта строчка целиком записана в файл
 
-// setTimeout(() => {
-//   limitedStream.write('world'); // ошибка LimitExceeded! в файле осталось только hello
-// }, 10);
+setTimeout(() => {
+  limitedStream.write('world'); // ошибка LimitExceeded! в файле осталось только hello
+}, 10);
