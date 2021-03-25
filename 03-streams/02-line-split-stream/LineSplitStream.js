38ca24ad9d8b144cfb05f9f6df8767a16bca8a77
Kozlov Dmitriy <siderwinder@icloud.com>
2021-03-25 13:43:23 +0300

    HW #3 task 2

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,36 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.remainder = '';
   }
 
   _transform(chunk, encoding, callback) {
+    const rawArray = chunk.toString().split(os.EOL);
+
+    rawArray.forEach((item, index, arr) => {
+      if (arr.length > 1){
+        switch (index){
+          case 0:
+            this.push(this.remainder + item)
+            this.remainder = '';
+            break
+          case arr.length - 1:
+            this.remainder = item;
+            break
+          default:
+            this.push(item)
+        }
+      } else {
+        this.remainder += item;
+      }
+    });
+    callback();
   }
 
   _flush(callback) {
+    this.push(this.remainder);
+    this.remainder = '';
+    callback();
   }
 }
 
