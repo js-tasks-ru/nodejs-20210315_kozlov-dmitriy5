c6cb06cc54e23a57701e97caf0a71ca6e31f5b5b
Kozlov Dmitriy <siderwinder@icloud.com>
2021-03-30 15:10:40 +0300

    HW #5 task 3

--- 05-http-server-streams/03-file-server-delete/server.js
+++ 05-http-server-streams/03-file-server-delete/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -11,6 +12,27 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'DELETE':
+      if (path.parse(filepath).dir != path.join(__dirname, 'files')){
+        res.statusCode = 400;
+        res.end('Server doesn\'t support inserts dir');
+        return
+      }
+      fs.stat(filepath, (err, stat) => {
+        if (err || !stat.isFile()){
+          res.statusCode = 404;
+          res.end('File hasn\'t found');
+        } else {
+          fs.unlink(filepath, (err) => {
+            if (err) {
+              res.statusCode = 500;
+              res.end('Internal server error');
+            } else {
+              res.statusCode = 200;
+              res.end('Succesful delete');
+            }
+          })
+        }
+      });
 
       break;
 
