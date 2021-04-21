6d20bf33e8dc840b6ed6459ed9f38b59d2d23626
Dmitriy <siderwinder@icloud.com>
2021-04-21 20:36:57 +0300

    HW #8 task 1

--- 08-authentication/01-local/libs/strategies/local.js
+++ 08-authentication/01-local/libs/strategies/local.js
@@ -1,8 +1,21 @@
 const LocalStrategy = require('passport-local').Strategy;
+const userModel = require('../../models/User');
+
 
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
+    async function(email, password, done) {
+      const user = await userModel.findOne({email: email}).exec();
+      if (!user){
+        done(null, false, "Нет такого пользователя");  
+      } else {
+        if (await user.checkPassword(password)){
+          done(null, user);
+        } else {
+          done(null, false, 'Неверный пароль');  
+        }
+      }
+      // console.log('The end');
+      // done(null, false, 'Стратегия подключена, но еще не настроена');  
     },
 );
